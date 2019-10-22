88b329dee7dcb22df3fc57c18a8fe88f3e59f4d4
digitalfabric <dwawryko@gmail.com>
2019-10-22 18:22:34 -0400

    Updated App.js

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+        φ
+        </p>
+        <a
+          className="App-link"
+          href="https://linktr.ee/digitalfabric"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+        Digital Fabric        
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
